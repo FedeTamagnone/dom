aae4730ee85e33197a3070ddba93ae62f35ab22e
FedeTamagnone <103047702+FedeTamagnone@users.noreply.github.com>
2022-06-20 16:23:38 -0300

    Dom

---
+++ js/main.js
@@ -0,0 +1,67 @@
+const carritoFinal = []
+
+function producto() {
+    let listado = prompt("¿Qué producto quiere llevar?: \n 1- Jabon $10 \n 2- Crema $20 \n 3- Peine $30")
+    switch (listado) {
+        case "1":
+            let cantidad = prompt("¿Cuántos Jabones queres llevar?")
+            let precio = (cantidad * 10)
+            const carrito = {
+                producto: "Jabón",
+                cantidad: cantidad,
+                total: precio
+            }
+            carritoFinal.push(carrito)
+            break;
+
+        case "2":
+            let cantidad1 = prompt("¿Cuántas cremas queres llevar?")
+            let precio1 = (cantidad1 * 20);
+
+            const carrito1 = {
+                producto: "Crema",
+                cantidad: cantidad1,
+                total: precio1
+            }
+            carritoFinal.push(carrito1)
+            break;
+
+        case "3":
+            let cantidad2 = prompt("¿Cuántos peines queres llevar?")
+            let precio2 = (cantidad2 * 30);
+
+            const carrito2 = {
+                producto: "Peine",
+                cantidad: cantidad2,
+                total: precio2
+            }
+            carritoFinal.push(carrito2)
+            break;
+    }
+    let agregarListado = prompt("¿Querés agregar otro producto SI/NO?")
+    if (agregarListado == "si") {
+        return producto();
+    } else {
+        console.log(carritoFinal);
+    }
+}
+
+producto()
+
+const precioFinal = carritoFinal.reduce((acc, item) => {
+    return acc += item.total;
+}, 0)
+
+
+for (const producto of carritoFinal) {
+    let totalCompra = document.createElement("div");
+    totalCompra.innerHTML = `<h3>Producto: ${producto.producto}</h3>
+        <p> Cantidad: ${producto.cantidad} </p>
+        <p> Precio Total: $ ${producto.total};`
+
+    document.body.append(totalCompra);
+}
+
+const precioFinalCompra = document.createElement("div");
+precioFinalCompra.innerHTML = `<h3> El total de su compra es de: $ ${precioFinal}`
+document.body.append(precioFinalCompra);
